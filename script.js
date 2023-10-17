10921a8e53464a293cb6bc2ca31f8fa3dda548b6
Pravin Kumar <109470708+pravinkumarone@users.noreply.github.com>
2023-10-18 00:44:37 +0530

    added rock, paper and scissor emoji and display score and result

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const container = document.querySelector(".container");
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissor = document.querySelector(".scissor");
+const result = document.querySelector('.result');
+const score = document.querySelector('.score');
 
 rock.addEventListener("click", function(){
 	playRound("rock",getComputerChoice())
@@ -17,32 +20,34 @@ function getComputerChoice(){
 	return play[Math.floor(Math.random() * 3)];
 }
 
+let computer = 0;
+let you = 0;
 
 function playRound(playerSelection, computerSelection){
 	playerSelection.toLowerCase();
 
 	if(playerSelection == computerSelection){
 		
-		console.log(`Computer: ${computerSelection} & You: ${playerSelection}`);
-		console.log(`It is a tie! both are ${playerSelection}s`);
+		// console.log();
+		result.innerText = "It is a tie! both are " + playerSelection + "s";
+		result.style.backgroundColor = 'grey';
 
 	} else if((playerSelection == "rock" && computerSelection == "paper") || 
 		(playerSelection == "scissor" && computerSelection == "rock") || 
 		(playerSelection == "paper" && computerSelection == "scissor")){
 		
-		console.log(`Computer: ${computerSelection} & You: ${playerSelection}`);
-		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+		result.innerHTML = "You Lose! " + computerSelection + " beats " + playerSelection;
+		result.style.backgroundColor = "red";
+		computer++;
+		score.innerText = `You: ${you} \t Computer: ${computer}`;
 
 	} else if((playerSelection == "rock" && computerSelection == "scissor") ||
 		(playerSelection == "paper" && computerSelection == "rock") ||
 		(playerSelection == "scissor" && computerSelection == "paper")){
 		
-		console.log(`Computer: ${computerSelection} & You: ${playerSelection}`);
-		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+		result.innerHTML = "You Win! " + playerSelection + " beats " + computerSelection;
+		result.style.backgroundColor = "green";
+		you++;
+		score.innerText = `You: ${you} \t Computer: ${computer}`;
 	}
 }
-
-	let win = 0;
-	let lose = 0;
-	let tie = 0;
-
