9921cd10d73afa4bc8f5e93c0bc93ced9ff8cd82
Pravin Kumar <109470708+pravinkumarone@users.noreply.github.com>
2023-10-17 23:28:10 +0530

    added logic to the buttons

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+
+rock.addEventListener("click", function(){
+	playRound("rock",getComputerChoice())
+});
+paper.addEventListener('click', function(){
+	playRound("paper",getComputerChoice());
+});
+scissor.addEventListener('click', function(){
+	playRound("scissor", getComputerChoice());
+});
+
 function getComputerChoice(){
 	const play = ["rock","paper","scissor"];		
 	return play[Math.floor(Math.random() * 3)];
-
 }
+
 
 function playRound(playerSelection, computerSelection){
 	playerSelection.toLowerCase();
@@ -11,7 +25,6 @@ function playRound(playerSelection, computerSelection){
 		
 		console.log(`Computer: ${computerSelection} & You: ${playerSelection}`);
 		console.log(`It is a tie! both are ${playerSelection}s`);
-		return 0;
 
 	} else if((playerSelection == "rock" && computerSelection == "paper") || 
 		(playerSelection == "scissor" && computerSelection == "rock") || 
@@ -19,7 +32,6 @@ function playRound(playerSelection, computerSelection){
 		
 		console.log(`Computer: ${computerSelection} & You: ${playerSelection}`);
 		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-		return -1;
 
 	} else if((playerSelection == "rock" && computerSelection == "scissor") ||
 		(playerSelection == "paper" && computerSelection == "rock") ||
@@ -27,30 +39,10 @@ function playRound(playerSelection, computerSelection){
 		
 		console.log(`Computer: ${computerSelection} & You: ${playerSelection}`);
 		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-		return 1;
 	}
 }
 
-function game(){
 	let win = 0;
 	let lose = 0;
 	let tie = 0;
 
-	for(let i = 1; i<= 5; i++){
-		let ans = playRound(prompt("Enter any"),getComputerChoice());
-		if(ans == 0){
-			tie++;
-		} else if(ans == 1){
-			win++;
-		} else if(ans == -1){
-			lose++;
-		}
-	}
-	if(win > lose){
-		console.log(`Congratulations! You are the winner with ${win} wins and ${lose} lose` );
-	} else if(lose > win){
-		console.log(`We're sorry, You lost the game with ${lose} lose and ${win} wins`);
-	}
-}
-
-game();
